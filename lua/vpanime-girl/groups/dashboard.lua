9529922d61c0568ad48935b4326736a1d6c66a1b
HabielLau <tcwilliamson86@gmail.com>
2025-06-02 18:43:58 -0400

    reshaded dashboard

--- lua/vpanime-girl/groups/dashboard.lua
+++ lua/vpanime-girl/groups/dashboard.lua
@@ -6,10 +6,10 @@ function M.get(c, opts)
     DashboardShortCut = { fg = c.cyan },
     DashboardHeader   = { fg = c.pink },
     DashboardCenter   = { fg = c.dkmagenta },
-    DashboardFooter   = { fg = c.purple, italic = true },
-    DashboardKey      = { fg = c.ltpurple500 },
-    DashboardDesc     = { fg = c.cyan500 },
-    DashboardIcon     = { fg = c.cyan500, bold = true },
+    DashboardFooter   = { fg = c.purple100, italic = true },
+    DashboardKey      = { fg = c.dkmagenta100 },
+    DashboardDesc     = { fg = c.cyan100 },
+    DashboardIcon     = { fg = c.cyan100, bold = true },
   }
 end
 
