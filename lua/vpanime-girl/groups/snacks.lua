0e55d13ae309453d44173dcf50586bc05d76420f
HabielLau <tcwilliamson86@gmail.com>
2025-06-04 22:16:20 -0400

    Revert "reshaded snacks dashboard special"
    This reverts commit 1967afadce7b29c3b07d73fbb23eced49ada6ab2.

--- lua/vpanime-girl/groups/snacks.lua
+++ lua/vpanime-girl/groups/snacks.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.get(c, opts)
   -- stylua: ignore
   return {
+<<<<<<< HEAD
     SnacksDashboardDesc       = { fg = c.cyan500 },
     SnacksDashboardKey        = { fg = c.orange500 },
     SnacksDashboardFooter     = { fg = c.yellow, italic = true },
@@ -13,6 +14,29 @@ function M.get(c, opts)
     SnacksNotifierIconTrace   = { fg = c.magenta500 },
     SnacksNotifierTitleTrace  = { fg = c.magenta500 },
     SnacksIndentScope         = { fg = c.violet700, nocombine = true },
+||||||| 1967afa (reshaded snacks dashboard special)
+    SnacksDashboardDesc       = { fg = c.cyan100 },
+    SnacksDashboardKey        = { fg = c.cyan },
+    SnacksDashboardFooter     = { fg = c.purple100, italic = true },
+    SnacksDashboardSpecial    = { fg = c.dkpink100 },
+    SnacksDashboardHeader     = { fg = c.pink },
+    SnacksDashboardIcon       = { fg = c.cyan100, bold = true },
+    SnacksNotifierBorderTrace = { fg = c.dkmagenta500 },
+    SnacksNotifierIconTrace   = { fg = c.dkmagenta500 },
+    SnacksNotifierTitleTrace  = { fg = c.dkmagenta500 },
+    SnacksIndentScope         = { fg = c.ltpink700, nocombine = true },
+=======
+    SnacksDashboardDesc       = { fg = c.cyan100 },
+    SnacksDashboardKey        = { fg = c.cyan },
+    SnacksDashboardFooter     = { fg = c.purple100, italic = true },
+    SnacksDashboardSpecial    = { fg = c.dkpink },
+    SnacksDashboardHeader     = { fg = c.pink },
+    SnacksDashboardIcon       = { fg = c.cyan100, bold = true },
+    SnacksNotifierBorderTrace = { fg = c.dkmagenta500 },
+    SnacksNotifierIconTrace   = { fg = c.dkmagenta500 },
+    SnacksNotifierTitleTrace  = { fg = c.dkmagenta500 },
+    SnacksIndentScope         = { fg = c.ltpink700, nocombine = true },
+>>>>>>> parent of 1967afa (reshaded snacks dashboard special)
     SnacksIndent              = { fg = c.base03, nocombine = true },
     SnacksPickerBorder        = { fg = c.base02, bg = c.bg_float },
     SnacksPickerTitle         = "Title",
